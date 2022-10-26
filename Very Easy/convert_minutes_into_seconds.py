571ab751e1b80ce7ac02f7e05935e82c5bd96506
pdaambrosio <pda.ambrosio@gmail.com>
2022-10-26 07:57:55 -0300

    feat: convert minutes

---
+++ Very Easy/convert_minutes_into_seconds.py
@@ -0,0 +1,12 @@
+def convert(minutes: int) -> int:
+    return minutes * 60
+
+
+def main():
+    print(convert(5))
+    print(convert(3))
+    print(convert(2))
+
+
+if __name__ == '__main__':
+    main()
